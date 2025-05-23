880a1b6aca5593ca170d8b2e9802a1098b7bbf5e
Gabi Baruzzo <gabriela.baruzzo@gmail.com>
2025-05-23 14:37:08 -0300

    Primer commit proyecto - Basico,prueba de herramientas

---
+++ app/Views/front/principal.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>principal</title>
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+  </head>
+  <body>
+    <h1>Tython is comming</h1>
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
+  </body>
+</html>
